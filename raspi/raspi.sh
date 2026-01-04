85f2dabc164e2e33d4df965ba3c703383274e2b4
Mason Perdue <220426478+masonperdue@users.noreply.github.com>
2026-01-03 16:44:56 -0800

    Update firewall rules for Caddy server

--- raspi/raspi.sh
+++ raspi/raspi.sh
@@ -102,10 +102,9 @@
     sudo firewall-cmd --zone=drop --add-port=7583/tcp   # ssh
     sudo firewall-cmd --zone=drop --add-port=53/tcp     # pi-hole dns
     sudo firewall-cmd --zone=drop --add-port=53/udp     # pi-hole dns
-    sudo firewall-cmd --zone=drop --add-port=80/tcp     # httpd
-    sudo firewall-cmd --zone=drop --add-port=443/tcp    # pi-hole admin
-    sudo firewall-cmd --zone=drop --add-port=8080/tcp   # miniflux
-    sudo firewall-cmd --zone=drop --add-port=8888/tcp   # joplin webdav
+    sudo firewall-cmd --zone=drop --add-port=80/tcp     # caddy
+    sudo firewall-cmd --zone=drop --add-port=443/tcp    # caddy
+    sudo firewall-cmd --runtime-to-permanent
     sudo firewall-cmd --state
     sudo firewall-cmd --get-default-zone
     sudo firewall-cmd --get-active-zones
