59ea0c8fef6cf30763b9d7ff2dee4b2755f4dd1a
Mason Perdue <220426478+masonperdue@users.noreply.github.com>
2026-01-05 13:43:16 -0800

    add setup.sh to link files

---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+# Setup Script for Linux Config
+# sudo apt install -y git && mkdir ~/.myconfig && cd ~/.myconfig && git clone https://github.com/masonperdue/linuxconfig.git && cd linux-config && chmod +x setup.sh && ./setup.sh
+
+echo "" >> /home/masonp/.bashrc
+echo ". /home/masonp/.bashrc.d/*" >> /home/masonp/.bashrc
+mkdir /home/masonp/.bashrc.d
+ln -sf /home/masonp/.myconfig/linux-config/bashrc.d/* /home/masonp/.bashrc.d/
+echo "source /home/masonp/.bashrc"
