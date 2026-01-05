e7e98f46ee66c2bd9acae546069d8be99158309c
Mason Perdue <220426478+masonperdue@users.noreply.github.com>
2026-01-05 14:08:47 -0800

    back up .ssh/config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 # Setup Script for Linux Config
 # sudo apt install -y git && mkdir ~/.myconfig && cd ~/.myconfig && git clone https://github.com/masonperdue/linuxconfig.git && cd linux-config && chmod +x setup.sh && ./setup.sh
 
+mkdir /home/masonp/.ssh
+ln -sf /home/masonp/.myconfig/ssh/config /home/masonp/.ssh/config
+chmod 600 /home/masonp/.ssh/config
+
 echo "" >> /home/masonp/.bashrc
 echo ". /home/masonp/.bashrc.d/*" >> /home/masonp/.bashrc
 mkdir /home/masonp/.bashrc.d
